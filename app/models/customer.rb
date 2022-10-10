02ce41ca8e34790246c3673cb88bc3c82f6612df
nyaaaaaaaaaaaaaa <cats.s2.257@gmail.com>
2022-10-10 11:18:05 +0000

    add

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -11,7 +11,7 @@ class Customer < ApplicationRecord
 
   #ゲストログイン機能
   def self.guest
-    find_or_create_by!(name: 'ゲスト', email: 'guest@example.com') do |customer|
+    find_or_create_by(name: 'ゲスト', email: 'guest@example.com') do |customer|
       customer.password = SecureRandom.urlsafe_base64
       # user.confirmed_at = Time.now  # Confirmable を使用している場合は必要
       # 例えば name を入力必須としているならば， user.name = "ゲスト" なども必要
